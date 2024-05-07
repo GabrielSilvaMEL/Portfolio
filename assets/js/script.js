9aaf4a506b5be5d9cc090482a02bd83f886a7802
Gabriel Silva <trotuman3000@outlook.com>
2024-05-07 10:31:06 -0300

    -Estilo atualizado -Informações atualizadas -Alinhamento melhorado -add bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,22 @@
-/*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'fa-sun'
+const themeButton = document.getElementById('theme-button');
+const html = document.documentElement;
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme)
-}
-
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+    // Alternar entre os temas
+    html.classList.toggle('dark-theme');
 
+    // Salvar o tema atual no armazenamento local
+    const isDarkTheme = html.classList.contains('dark-theme');
+    localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+});
+
+// Verificar se o usuário já selecionou um tema anteriormente
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+    // Aplicar o tema salvo
+    html.classList.toggle(savedTheme);
+}
 /*==================== Projects SHOW ====================*/
 function exibirPrimeiraImagem() {
     var images = document.querySelectorAll(".project-image");
@@ -35,19 +24,19 @@ function exibirPrimeiraImagem() {
 }
 
 function mostrarProjeto(projeto) {
-    var nomeProjeto = document.querySelector(".project-details h1");
-    var subDesc = document.querySelector(".project-details h2");
+    var nomeProjeto = document.querySelector(".project-details h3");
+    var subDesc = document.querySelector(".project-details h5");
     var descricaoProjeto = document.querySelector(".project-details p");
     var projectImages = document.querySelector(".project-images");
     var projectlink = document.querySelector(".project-details a");
-    var projectTags = document.querySelector(".project-details h3");
+    var projectTags = document.querySelector(".project-details h6");
     switch (projeto) {
         case 1:
             nomeProjeto.innerHTML = "AstroShows";
             subDesc.innerHTML = "Plataforma de venda de ingressos";
-            descricaoProjeto.innerHTML = "<li><b>Painel Administrativo<b></li><li>Autenticação de usuário</li> <li>Design minimalista, moderno e funcional</li>";
+            descricaoProjeto.innerHTML = "<li>Painel Administrativo</li><li>Autenticação de usuário</li> <li>Design minimalista, moderno e funcional</li>";
             projectlink.innerHTML = `
-                        <a class="projects-link color-first" href="https://www.linkedin.com/posts/gabrielsilvalves331_javascript-css-php-activity-7017916449834868736-elb4?utm_source=share&utm_medium=member_desktop" target="_blank"><i class="fas fa-external-link-alt"></i> Ver <span class="color-first">AstroShows</span></a>
+                        <a class="projects-link color-first " href="https://www.linkedin.com/posts/gabrielsilvalves331_javascript-css-php-activity-7017916449834868736-elb4?utm_source=share&utm_medium=member_desktop" target="_blank"><i class="fas fa-external-link-alt"></i> Ver <span class="color-first">AstroShows</span></a>
                     `;
             projectTags.innerHTML = `
                         <div class="project-tags">
@@ -59,16 +48,16 @@ function mostrarProjeto(projeto) {
                     `;
             projectImages.innerHTML = `
             <div class="carousel">
-                <div><img class="project-image" src="img/astroshows1.png" alt="Projeto 1 Imagem 1"></div>
-                <div><img class="project-image" src="img/astroshows2.png" alt="Projeto 1 Imagem 2"></div>
-                <div><img class="project-image" src="img/astroshows3.png" alt="Projeto 1 Imagem 3"></div>
+                <img class="project-image col-auto" src="img/astroshows1.png" alt="Projeto 1 Imagem 1">
+                <img class="project-image col-auto" src="img/astroshows2.png" alt="Projeto 1 Imagem 2">
+                <img class="project-image col-auto" src="img/astroshows3.png" alt="Projeto 1 Imagem 3">
             </div>
         `;
             break;
         case 2:
             nomeProjeto.innerHTML = "KalderBlog";
             subDesc.innerHTML = "Blog sobre a Indústria Musical";
-            descricaoProjeto.innerHTML = "Projeto feito com maior atenção ao front-end.<li><b>Painel Administrativo<b></li><li>Autenticação de usuário</li> <li>Design minimalista, moderno e funcional</li>";
+            descricaoProjeto.innerHTML = "Projeto feito com maior atenção ao front-end.<li>Painel Administrativo<li>Design minimalista, moderno e funcional</li>";
             projectlink.innerHTML = `<a class="projects-link color-first" href="https://www.linkedin.com/posts/gabrielsilvalves331_vite-laravel-frontend-activity-7111082136538918912-oiDO?utm_source=share&utm_medium=member_desktop" target="_blank"><i class="fas fa-external-link-alt"></i> Ver <span class="color-first">KalderBlog</span></a>`;
             projectTags.innerHTML = `
                         <div class="project-tags">
@@ -79,11 +68,14 @@ function mostrarProjeto(projeto) {
                         </div>
                     `;
             projectImages.innerHTML = `
+            <div class="carousel mb-1">
+               <img class="project-image col-auto" src="img/kalderblog1.png" alt="Projeto 1 Imagem 1">
+               <img class="project-image col-auto" src="img/kalderblog2.png" alt="Projeto 1 Imagem 2">
+            </div>
+            
             <div class="carousel">
-                <div><img class="project-image" src="img/kalderblog1.png" alt="Projeto 1 Imagem 1"></div>
-                <div><img class="project-image" src="img/kalderblog2.png" alt="Projeto 1 Imagem 2"></div>
-                <div><img class="project-image" src="img/kalderblog4.png" alt="Projeto 1 Imagem 3"></div>
-                <div><img class="project-image" src="img/kalderblog5.png" alt="Projeto 1 Imagem 3"></div>
+               <img class="project-image col-auto" src="img/kalderblog4.png" alt="Projeto 1 Imagem 3">
+               <img class="project-image col-auto" src="img/kalderblog5.png" alt="Projeto 1 Imagem 3">
             </div>
         `;
             break;
@@ -101,9 +93,8 @@ function mostrarProjeto(projeto) {
                         `;
             projectImages.innerHTML = `
             <div class="carousel">
-                <div><img class="project-image" src="img/darkwood1.png" alt="darkwood1"></div>
-                <div><img class="project-image" src="img/darkwood2.png" alt="Projeto 3 Imagem 2"></div>
-                
+                <img class="project-image col-auto" src="img/darkwood1.png" alt="darkwood1">
+                <img class="project-image col-auto" src="img/darkwood2.png" alt="Projeto 3 Imagem 2">
             </div>
         `;
         break;
@@ -120,14 +111,14 @@ function mostrarProjeto(projeto) {
                         `;
             projectImages.innerHTML = `
             <div class="carousel">
-                <div><img class="project-image" src="img/javascript.png" alt="javascrip1"></div>
+               <img class="project-image col-auto" src="img/javascript.png" alt="javascrip1">
             </div>
         `;
             break;
         case 5:
             nomeProjeto.innerHTML = "Portfólio";
             subDesc.innerHTML = "Meu Portfólio Responsivo";
-            descricaoProjeto.innerHTML = "Feito <b>sem Bootstrap</b>, com um design moderno e profissional. Nas fotos abaixo é possível conferir o design antigo do meu portfólio e notar a minha evolução como programador.<br> Dark/Light Theme";
+            descricaoProjeto.innerHTML = "Design moderno e profissional. Nas fotos abaixo é possível conferir o design antigo do meu portfólio e notar a minha evolução como programador.<br> Dark/Light Theme";
             projectlink.innerHTML = `<a class="projects-link color-first" href="https://gabrielsilvamel.github.io/Portfolio" target="_blank"><i class="fas fa-external-link-alt"></i> Ver <span class="color-first">Portfólio</span></a>`;
             projectTags.innerHTML = `
                             <div class="project-tags">
@@ -137,14 +128,14 @@ function mostrarProjeto(projeto) {
                         `;
             projectImages.innerHTML = `
             <div class="carousel">
-                <div><img class="project-image" src="img/portfolio.png" alt="darkwood1"></div>
-                <div><img class="project-image" src="img/portfolioantigo.png" alt="Projeto 3 Imagem 2"></div>
+                <img class="project-image col-auto" src="img/portfolio.png" alt="darkwood1">
+                <img class="project-image col-auto" src="img/portfolioantigo.png" alt="Projeto 3 Imagem 2">
             </div>
         `;
             break;
     }
     
-    var overlays = document.querySelectorAll(".overlay");
+    var overlays = document.querySelectorAll(".card");
     overlays.forEach(function (overlay, index) {
         overlay.addEventListener('click', function () {
             exibirPrimeiraImagem(); // Chama a função para exibir a primeira imagem
@@ -152,58 +143,4 @@ function mostrarProjeto(projeto) {
     });
 
 }
-
-/*==================== Projects Swiper ====================*/
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    centeredSlides: false,
-    spaceBetween: 10,
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-
-var appendNumber = 4;
-var prependNumber = 1;
-document
-    .querySelector(".prepend-2-slides")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide([
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-        ]);
-    });
-document
-    .querySelector(".prepend-slide")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.prependSlide(
-            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-        );
-    });
-document
-    .querySelector(".append-slide")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide(
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-        );
-    });
-document
-    .querySelector(".append-2-slides")
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        swiper.appendSlide([
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-        ]);
-    });
-
 
