8f63e105591d57e2072ac310bb7707d393f17d68
Gabriel Silva <trotuman3000@outlook.com>
2024-11-10 09:07:32 -0300

    CSS>Projects

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,45 +19,45 @@ const Navbar = () =>{
       }
   };
     return(
-        <div className="d-flex  align-items-center text-center justify-content-around mt-4">
-    <span className="fw-600">Gabriel</span>
-    <div className="row gap-0 fw-600">
-    <a className="col-auto" href="#projects">Projetos</a>
-    <a className="col-auto" href="#certificados">Certificados</a>
-    <a className="col-auto" href="#exp">Experiências</a>
+    <div className="d-flex  align-items-center text-center justify-content-around mt-4">
+      <span className="fw-600">Gabriel</span>
+        <div className="row gap-0 fw-600">
+        <a className="col-auto" href="#projects">Projetos</a>
+        <a className="col-auto" href="#certificados">Certificados</a>
+        <a className="col-auto" href="#exp">Experiências</a>
 
-    <div className="d-flex flex-row col-auto gap-16 align-items-center text-center justify-content-center contato-div">
-      <a href="https://www.linkedin.com/in/gabrielsilvalves331/" title="LinkedIn"
-         className="fab fa-linkedin-in" target="_blank"></a>
-      <a href="https://github.com/gabrielsilvamel" title="Github" className="fab fa-github"
-         target="_blank"></a>
-      <a href="mailto:gabrielsilvaalves3000@outlook.com" title="Enviar Email" className="far fa-envelope"
-         target="_blank"></a>
-    </div>
-
-    <a className="col-auto" onClick={toggleTheme}>
-      <i className={isDarkMode ? 'far fa-sun' : 'far fa-moon'}></i>
-      {isDarkMode ? 'Light' : 'Dark'} Mode
-    </a>
-
-    {/* Dropdown de Currículo */}
-    <div className="dropdown col-auto">
-      <a className="hire-me dropdown-toggle" onClick={toggleDropdown} title="Baixar Currículo">
-        <i className="fas fa-download"></i> Currículo
-      </a>
-      {isDropdownOpen && (
-        <div className="dropdown-menu show">
-          <a href="/curriculoGabriel.pdf" download="curriculoGabriel.pdf" className="dropdown-item">
-            <img src="/Flag_BR.png" alt="Portuguese Flag" className="flag-icon" /> Português
-          </a>
-          <a href="/resumeGabriel.pdf" download="resumeGabriel.pdf" className="dropdown-item">
-            <img src="Flag_USA.png" alt="English Flag" className="flag-icon" /> English
-          </a>
+        <div className="d-flex flex-row col-auto gap-16 align-items-center text-center justify-content-center contato-div">
+          <a href="https://www.linkedin.com/in/gabrielsilvalves331/" title="LinkedIn"
+            className="fab fa-linkedin-in" target="_blank"></a>
+          <a href="https://github.com/gabrielsilvamel" title="Github" className="fab fa-github"
+            target="_blank"></a>
+          <a href="mailto:gabrielsilvaalves3000@outlook.com" title="Enviar Email" className="far fa-envelope"
+            target="_blank"></a>
         </div>
-      )}
+
+        <a className="col-auto" onClick={toggleTheme}>
+          <i className={isDarkMode ? 'far fa-sun' : 'far fa-moon'}></i>
+          {isDarkMode ? 'Light' : 'Dark'} Mode
+        </a>
+
+        {/* Dropdown de Currículo */}
+        <div className="dropdown col-auto">
+          <a className="hire-me dropdown-toggle" onClick={toggleDropdown} title="Baixar Currículo">
+            <i className="fas fa-download"></i> Currículo
+          </a>
+          {isDropdownOpen && (
+            <div className="dropdown-menu show">
+              <a href="/curriculoGabriel.pdf" download="curriculoGabriel.pdf" className="dropdown-item">
+                <img src="/Flag_BR.png" alt="Portuguese Flag" className="flag-icon" /> Português
+              </a>
+              <a href="/resumeGabriel.pdf" download="resumeGabriel.pdf" className="dropdown-item">
+                <img src="Flag_USA.png" alt="English Flag" className="flag-icon" /> English
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
     )
 }
